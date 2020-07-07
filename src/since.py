8f3e5d06c487bc50eccab231f127e702bb926755
Matt Cooper <mattc@xbox.com>
2020-07-07 07:01:17 -0400

    render a homepage

--- src/since.py
+++ src/since.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    return render_template('home.html')
 
 
 @app.route("/<historical_date>")
