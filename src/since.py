001af2eece3794e1f4ce75c9a92bcb837da3982f
Matt Cooper <mattc@xbox.com>
2020-07-07 06:50:16 -0400

    return days since a given date

--- src/since.py
+++ src/since.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +7,20 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route("/<historical_date>")
+def get_since(historical_date):
+    try:
+        parsed_date = datetime.strptime(historical_date, '%Y-%m-%d')
+    except ValueError as e:
+        return f"Could not parse date: {e}", 400
+    
+    actual_date = parsed_date.date()
+    today = datetime.utcnow().date()
+    days_since = today - actual_date
+
+    if days_since.days <= 0:
+        return f"Must pick a date before {today}", 400
+
+    return str(days_since.days)
