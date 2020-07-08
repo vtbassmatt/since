38a029fed1c9a0d175c182f632cc61b650fb6efa
Matt Cooper <mattc@xbox.com>
2020-07-07 21:09:40 -0400

    tell you how old you are relative to major events

--- src/since.py
+++ src/since.py
@@ -1,26 +1,75 @@
-from datetime import datetime
+from datetime import date, datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+SOME_HISTORICAL_DATES = [
+    (date(1930, 7, 7), 'the death of Arthur Conan Doyle'),
+    (date(1945, 9, 2), 'the official end of World War II'),
+    (date(1947, 8, 15), 'Indian independence from the United Kingdom'),
+    (date(1977, 8, 16), 'Elvis Presley\'s death'),
+    (date(2008, 11, 4), 'Barack Obama\'s first presidential election victory'),
+]
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
 
-@app.route("/<historical_date>")
-def get_since(historical_date):
+@app.route("/<request_date>")
+def get_since(request_date):
     try:
-        parsed_date = datetime.strptime(historical_date, '%Y-%m-%d')
+        parsed_date = datetime.strptime(request_date, '%Y-%m-%d')
     except ValueError as e:
-        return f"Could not parse date: {e}", 400
+        return api_error(f"Could not parse date: {e}"), 400
     
-    actual_date = parsed_date.date()
+    request_date_cleaned = parsed_date.date()
     today = datetime.utcnow().date()
-    days_since = today - actual_date
+    days_since = today - request_date_cleaned
 
     if days_since.days <= 0:
-        return f"Must pick a date before {today}", 400
+        return api_error(f"Must pick a date before {today}"), 400
+    
+    search_date = request_date_cleaned - days_since
 
-    return str(days_since.days)
+    response = {
+        'requested': {
+            'date': request_date_cleaned,
+            'days_since': days_since.days,
+        },
+        'historical_fact': {
+            'searched_date': search_date,
+        },
+    }
+
+    try:
+        historical_thing = find_historical_thing(SOME_HISTORICAL_DATES, search_date)
+        days_from_historical_to_request = (request_date_cleaned - historical_thing[0]).days
+
+        if days_from_historical_to_request >= 0:
+            response['historical_fact'].update({
+                'found_date': historical_thing[0],
+                'caption': historical_thing[1],
+                'days_from_this_to_requested': days_from_historical_to_request,
+            })
+
+    except ValueError:
+        pass
+
+    return jsonify(response)
+
+
+# dumbest possible implementation that works
+def find_historical_thing(items, search_date):
+    print(search_date)
+    for i in range(len(items)):
+        if items[i][0] > search_date:
+            return items[i]
+    raise ValueError("could not find a valid historical event")
+
+
+def api_error(message):
+    return jsonify({
+        "error": message,
+    })
