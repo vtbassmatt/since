23025ffa289b586a6f469ad6e6e8742484f17c62
Matt Cooper <vtbassmatt@gmail.com>
2020-07-19 07:15:33 -0400

    make scp actually scp

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -60,7 +60,7 @@ RESTART_CMD="ssh ${APP_USER}@$APP_HOST 'sudo systemctl restart since-app'"
 eval $RESTART_CMD
 
 echo "::::: List orphaned deploys"
-ORPHAN_SCP_CMD="scp -B ${PROJECT_DIR}deploy/lsorphans.sh ${APP_DIR}lsorphans.sh"
+ORPHAN_SCP_CMD="scp -B ${PROJECT_DIR}deploy/lsorphans.sh ${APP_USER}@${APP_HOST}:${APP_DIR}"
 ORPHAN_LS_CMD="ssh ${APP_USER}@$APP_HOST '${APP_DIR}lsorphans.sh ${APP_DIR}deploys/ | tee ${APP_DIR}orphans.txt'"
 eval $ORPHAN_SCP_CMD
 eval $ORPHAN_LS_CMD
