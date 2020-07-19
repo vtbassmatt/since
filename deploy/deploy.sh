b64bca4bf9eac07d137dca4f9491cae9b33833be
Matt Cooper <vtbassmatt@gmail.com>
2020-07-19 07:42:33 -0400

    Fix venv location and lsorphans script (#4)
    * move venv to deploys/
    * don't write orphans list to disk
    * fix lsorphans for Linux

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,7 +15,7 @@ APP_DIR="/data/since-app/"
 APP_USER="since"
 DEPLOY_NAME="$(date '+%Y-%m-%d-%H-%M-%S')-$(git rev-parse --short HEAD)"
 DEPLOY_DIR="${APP_DIR}deploys/${DEPLOY_NAME}/"
-VENV_DIR="${APP_DIR}.env-${DEPLOY_NAME}"
+VENV_DIR="${APP_DIR}deploys/.env-${DEPLOY_NAME}"
 SCP_TARGET="${APP_USER}@${APP_HOST}:${DEPLOY_DIR}"
 
 echo "::::: Creating target directory"
@@ -61,6 +61,6 @@ eval $RESTART_CMD
 
 echo "::::: List orphaned deploys"
 ORPHAN_SCP_CMD="scp -B ${PROJECT_DIR}deploy/lsorphans.sh ${APP_USER}@${APP_HOST}:${APP_DIR}"
-ORPHAN_LS_CMD="ssh ${APP_USER}@$APP_HOST '${APP_DIR}lsorphans.sh ${APP_DIR} | tee ${APP_DIR}orphans.txt'"
+ORPHAN_LS_CMD="ssh ${APP_USER}@$APP_HOST '${APP_DIR}lsorphans.sh ${APP_DIR}'"
 eval $ORPHAN_SCP_CMD
 eval $ORPHAN_LS_CMD
