d94787593be3e092129209367dd3f32d3c177ce0
Matt Cooper <vtbassmatt@gmail.com>
2020-07-19 07:08:55 -0400

    add lsorphans in testing mode

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -59,5 +59,8 @@ echo "::::: Restarting uWSGI"
 RESTART_CMD="ssh ${APP_USER}@$APP_HOST 'sudo systemctl restart since-app'"
 eval $RESTART_CMD
 
-# echo "::::: Get stats on orphaned deploys"
-# echo "TODO"
+echo "::::: List orphaned deploys"
+ORPHAN_SCP_CMD="scp -B ${PROJECT_DIR}deploy/lsorphans.sh ${APP_DIR}"
+ORPHAN_LS_CMD="ssh ${APP_USER}@$APP_HOST '${APP_DIR}lsorphans.sh ${APP_DIR}deploys/'"
+#eval $ORPHAN_SCP_CMD
+#eval $ORPHAN_LS_CMD
